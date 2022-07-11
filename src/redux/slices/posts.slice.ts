057ff19325532d01ed1e4ffd5b2ed4c3914819f6
Ali <alifarajzade1@yahoo.com>
2022-07-11 14:48:56 +0430

    Add reactions to initial state, prepare function and add function to create reactions.

--- src/redux/slices/posts.slice.ts
+++ src/redux/slices/posts.slice.ts
@@ -9,6 +9,13 @@ const initialState: TPost[] = [
         body: 'React is a JavaScript library for building user interfaces.',
         authorId: '1',
         createdAt: '2022-07-10T08:01:47.876Z',
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            rocket: 0,
+            heart: 0,
+            coffee: 0,
+        },
     },
     {
         id: '2',
@@ -16,6 +23,13 @@ const initialState: TPost[] = [
         body: 'Redux is a predictable state container for JavaScript apps.',
         authorId: '2',
         createdAt: '2022-07-11T07:25:47.876Z',
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            rocket: 0,
+            heart: 0,
+            coffee: 0,
+        },
     },
 ]
 
@@ -34,13 +48,31 @@ const postsSlice = createSlice({
                     id: nanoid(),
                     authorId,
                     createdAt: new Date().toISOString(),
+                    reactions: {
+                        thumbsUp: 0,
+                        wow: 0,
+                        rocket: 0,
+                        heart: 0,
+                        coffee: 0,
+                    },
                 },
             }),
+        },
+        addNewReaction: (
+            state,
+            action: PayloadAction<{
+                reaction: 'wow' | 'rocket' | 'thumbsUp' | 'coffee' | 'heart'
+                postId: string
+            }>
+        ) => {
+            const { postId, reaction } = action.payload
+            const postToReact = state.find(post => post.id === postId)!
+            postToReact.reactions[reaction]++
         },
     },
 })
 
 export const selectAllPosts = (state: TRootState) => state.posts
 
-export const { addNewPost } = postsSlice.actions
+export const { addNewPost, addNewReaction } = postsSlice.actions
 export default postsSlice.reducer
