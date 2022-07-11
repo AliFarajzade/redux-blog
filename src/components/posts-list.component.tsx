a29f27227e4ee5294667662218d9ddae4e7d9214
Ali <alifarajzade1@yahoo.com>
2022-07-11 12:51:50 +0430

    Add post author and created date to each post and sort posts based on created date.

--- src/components/posts-list.component.tsx
+++ src/components/posts-list.component.tsx
@@ -1,17 +1,27 @@
+import moment from 'moment'
 import { useSelector } from 'react-redux'
 import { selectAllPosts } from '../redux/slices/posts.slice'
 import { TRootState } from '../redux/store'
 import { TPost } from '../types/post.types'
+import PostAuthor from './post-author.component'
 
 const PostsList: React.FC = () => {
     const posts = useSelector<TRootState, TPost[]>(selectAllPosts)
 
+    const orderedPosts = posts
+        .slice()
+        .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
+
     return (
         <section>
             <h2>Posts</h2>
-            {posts.map(post => (
+            {orderedPosts.map(post => (
                 <article key={post.id}>
                     <h3>{post.title}</h3>
+                    <p className="postCredit">
+                        <PostAuthor userId={post.authorId} />
+                        <span>{moment(post.createdAt).fromNow()}</span>
+                    </p>
                     <p>{post.body}</p>
                 </article>
             ))}
