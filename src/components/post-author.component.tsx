cf068bcf3b5a0203baf68f0a5e9f37d35136bb80
Ali <alifarajzade1@yahoo.com>
2022-07-11 12:51:17 +0430

    Create component for post author.

---
+++ src/components/post-author.component.tsx
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux'
+import { selectAllUsers } from '../redux/slices/users.slice'
+import { TRootState } from '../redux/store'
+import { TUser } from '../types/user.types'
+
+interface IProps {
+    userId: string
+}
+
+const PostAuthor: React.FC<IProps> = ({ userId }) => {
+    const users = useSelector<TRootState, TUser[]>(selectAllUsers)
+
+    const author = users.find(user => user.id === userId)!
+
+    return <span>by {author.name}</span>
+}
+
+export default PostAuthor
