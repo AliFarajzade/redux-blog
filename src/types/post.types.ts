a434da1c9e80920c0e07070703c062122380d8fa
Ali <alifarajzade1@yahoo.com>
2022-07-11 14:47:54 +0430

    Add reactions field.

--- src/types/post.types.ts
+++ src/types/post.types.ts
@@ -1,7 +1,10 @@
+import { TReaction } from './reactions.types'
+
 export type TPost = {
     id: string
     title: string
     body: string
     authorId: string
     createdAt: string
+    reactions: TReaction
 }
