2ac71f76934b123b4f0f084771062aae0ff59a9f
Ali <alifarajzade1@yahoo.com>
2022-07-11 14:49:54 +0430

    Create reactions component.

---
+++ src/components/reaction-buttons.component.tsx
@@ -0,0 +1,61 @@
+import { useDispatch } from 'react-redux'
+import { addNewReaction } from '../redux/slices/posts.slice'
+import { TPost } from '../types/post.types'
+
+const reactionsEmojis = {
+    thumbsUp: '👍',
+    rocket: '🚀',
+    wow: '😮',
+    heart: '❤️',
+    coffee: '☕️',
+}
+
+interface IProps {
+    post: TPost
+}
+
+const ReactionButtons: React.FC<IProps> = ({ post }) => {
+    const dispatch = useDispatch()
+
+    const handleAddNewReaction = (
+        reaction: 'wow' | 'rocket' | 'thumbsUp' | 'coffee' | 'heart',
+        postId: string
+    ) => dispatch(addNewReaction({ postId, reaction }))
+
+    return (
+        <div>
+            {Object.entries(reactionsEmojis).map(([key, value]) => (
+                <button
+                    key={key}
+                    type="button"
+                    className="reactionButton"
+                    onClick={() =>
+                        handleAddNewReaction(
+                            key as
+                                | 'wow'
+                                | 'rocket'
+                                | 'thumbsUp'
+                                | 'coffee'
+                                | 'heart',
+                            post.id
+                        )
+                    }
+                >
+                    {value}{' '}
+                    {
+                        post.reactions[
+                            key as
+                                | 'wow'
+                                | 'rocket'
+                                | 'thumbsUp'
+                                | 'coffee'
+                                | 'heart'
+                        ]
+                    }
+                </button>
+            ))}
+        </div>
+    )
+}
+
+export default ReactionButtons
