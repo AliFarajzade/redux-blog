63954437e8272adca08e6f67965104eae9356b20
Ali <alifarajzade1@yahoo.com>
2022-07-11 11:54:58 +0430

    Add create new post component to app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import AddPost from './components/add-post.component'
 import PostsList from './components/posts-list.component'
 
 const App: React.FC = () => {
     return (
         <main className="App">
             <PostsList />
+            <AddPost />
         </main>
     )
 }
