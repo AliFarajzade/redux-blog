1c6508331972d53d5632fca8ef8e0b6b5342927b
Ali <alifarajzade1@yahoo.com>
2022-07-11 11:54:16 +0430

    Add action for creating new post.

--- src/redux/slices/posts.slice.ts
+++ src/redux/slices/posts.slice.ts
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit'
+import { TPost } from '../../types/post.type'
 import { TRootState } from '../store'
 
 const initialState = [
@@ -17,9 +18,23 @@ const initialState = [
 const postsSlice = createSlice({
     name: 'postsSlice',
     initialState,
-    reducers: {},
+    reducers: {
+        addNewPost: {
+            reducer: (state, action: PayloadAction<TPost>) => {
+                state.unshift(action.payload)
+            },
+            prepare: (title: string, body: string) => ({
+                payload: {
+                    title,
+                    body,
+                    id: nanoid(),
+                },
+            }),
+        },
+    },
 })
 
 export const selectAllPosts = (state: TRootState) => state.posts
 
+export const { addNewPost } = postsSlice.actions
 export default postsSlice.reducer
