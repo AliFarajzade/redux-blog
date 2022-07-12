d0d77ddb51d295c0c3d3c391e66bd162238b2b9f
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-12 11:12:00 +0430

    Create thunk action for getting users.

--- src/redux/slices/users.slice.ts
+++ src/redux/slices/users.slice.ts
@@ -1,26 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 import { TUser } from '../../types/user.types'
 import { TRootState } from '../store'
 
-const initialState: TUser[] = [
-    {
-        id: '1',
-        name: 'John Doe',
-    },
-    {
-        id: '2',
-        name: 'Rebbecca Bennett',
-    },
-    {
-        id: '3',
-        name: 'Jane Williams',
-    },
-]
+const BASE_URL = 'https://jsonplaceholder.typicode.com/users'
+
+const initialState: TUser[] = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(BASE_URL)
+    return response.data
+})
 
 const usersSlice = createSlice({
     name: 'usersSlice',
     initialState,
     reducers: {},
+    extraReducers: builder => {
+        builder.addCase(fetchUsers.fulfilled, (_state, action) => {
+            return action.payload
+        })
+    },
 })
 
 export const selectAllUsers = (state: TRootState) => state.users
