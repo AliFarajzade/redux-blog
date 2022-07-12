507cd2db130cd702b89091d9bc58c8541b88907e
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-12 10:59:46 +0430

    Replace logic with new thunk and UI with new component.

--- src/components/posts-list.component.tsx
+++ src/components/posts-list.component.tsx
@@ -1,32 +1,43 @@
-import moment from 'moment'
-import { useSelector } from 'react-redux'
-import { selectAllPosts } from '../redux/slices/posts.slice'
+import { nanoid } from '@reduxjs/toolkit'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+    fetchPosts,
+    selectAllPosts,
+    selectPostsError,
+    selectPostsStatus,
+} from '../redux/slices/posts.slice'
 import { TRootState } from '../redux/store'
 import { TPost } from '../types/post.types'
-import PostAuthor from './post-author.component'
-import ReactionButtons from './reaction-buttons.component'
+import { TRequestStatus } from '../types/request.types'
+import PostExcerpt from './post-excerpt.component'
 
 const PostsList: React.FC = () => {
-    const posts = useSelector<TRootState, TPost[]>(selectAllPosts)
+    const dispatch = useDispatch<any>()
 
-    const orderedPosts = posts
-        .slice()
-        .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
+    const posts = useSelector<TRootState, TPost[]>(selectAllPosts)
+    const postsStatus = useSelector<TRootState, TRequestStatus>(
+        selectPostsStatus
+    )
+    const postsError = useSelector<TRootState, any>(selectPostsError)
+
+    useEffect(() => {
+        if (postsStatus === 'idle') dispatch(fetchPosts())
+    }, [postsStatus, dispatch])
+
+    const orderedPosts =
+        postsStatus === 'succeeded' &&
+        posts.slice().sort((a, b) => b.createdAt.localeCompare(a.createdAt))
 
     return (
         <section>
             <h2>Posts</h2>
-            {orderedPosts.map(post => (
-                <article key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p className="postCredit">
-                        <PostAuthor userId={post.authorId} />
-                        <span>{moment(post.createdAt).fromNow()}</span>
-                    </p>
-                    <p>{post.body}</p>
-                    <ReactionButtons post={post} />
-                </article>
-            ))}
+            {postsStatus === 'loading' && <p>Loading...</p>}
+            {postsStatus === 'succeeded' &&
+                (orderedPosts as TPost[]).map(post => (
+                    <PostExcerpt key={nanoid()} post={post} />
+                ))}
+            {postsStatus === 'failed' && <p>{postsError}</p>}
         </section>
     )
 }
