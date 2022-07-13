6f48fa383dd14aacc25c71ecbbe6b3c5e01e578c
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-13 17:37:21 +0430

    Add logic to show post link on home page.

--- src/components/post-excerpt.component.tsx
+++ src/components/post-excerpt.component.tsx
@@ -1,20 +1,23 @@
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 import { TPost } from '../types/post.types'
 import PostAuthor from './post-author.component'
 import ReactionButtons from './reaction-buttons.component'
 
 interface IProps {
     post: TPost
+    isPostPage?: boolean
 }
 
-const PostExcerpt: React.FC<IProps> = ({ post }) => (
+const PostExcerpt: React.FC<IProps> = ({ post, isPostPage }) => (
     <article>
         <h3>{post.title}</h3>
+        <p className="excerpt">{post.body}</p>
         <p className="postCredit">
+            {!isPostPage && <Link to={`/post/${post.id}`}>Post Page</Link>}
             <PostAuthor userId={post.userId} />
             <span>{moment(post.createdAt).fromNow()}</span>
         </p>
-        <p>{post.body}</p>
         <ReactionButtons post={post} />
     </article>
 )
