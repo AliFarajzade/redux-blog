3d8e69cc4686a46458b7642bc7859ee76b8c928b
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-13 17:37:44 +0430

    Create routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import AddPost from './components/add-post.component'
+import Layout from './components/layout.component'
 import PostsList from './components/posts-list.component'
+import SinglePostPage from './components/single-post-page.component'
 
 const App: React.FC = () => {
     return (
-        <main className="App">
-            <AddPost />
-            <PostsList />
-        </main>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<PostsList />} />
+
+                <Route path="post">
+                    <Route index element={<AddPost />} />
+                    <Route path=":postId" element={<SinglePostPage />} />
+                </Route>
+            </Route>
+        </Routes>
     )
 }
 
