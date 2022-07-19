a48294b21f15b1fff919a4e06eb01c38fd539a51
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-19 09:45:26 +0430

    Add routes for new user components.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import EditPostForm from './components/edit-post.component'
 import Layout from './components/layout.component'
 import PostsList from './components/posts-list.component'
 import SinglePostPage from './components/single-post-page.component'
+import UserPage from './components/user.page'
+import UsersList from './components/users-list.component'
 
 const App: React.FC = () => {
     return (
@@ -11,11 +13,19 @@ const App: React.FC = () => {
             <Route path="/" element={<Layout />}>
                 <Route index element={<PostsList />} />
 
+                <Route path="users">
+                    <Route index element={<UsersList />} />
+                    <Route path=":userId" element={<UserPage />} />
+                </Route>
+
                 <Route path="post">
                     <Route index element={<AddPost />} />
                     <Route path="edit/:postId" element={<EditPostForm />} />
                     <Route path=":postId" element={<SinglePostPage />} />
                 </Route>
+
+                {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+                <Route path="*" element={<div>404</div>} />
             </Route>
         </Routes>
     )
