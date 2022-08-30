97aa6d43ada10be6f0143bf9c21b72df516eca1c
Ali Farajzade <alifarajzade1@yahoo.com>
2022-08-30 12:47:10 +0430

    Add stylings.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 const store = configureStore({
     reducer: {},
@@ -11,5 +11,6 @@ export type RootState = ReturnType<typeof store.getState>
 // Typed hooks
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export default store
